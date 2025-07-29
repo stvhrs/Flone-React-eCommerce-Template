7f4b6e28175de1ca451cb9f10d5b0d3024887945
stvhrs <stvhrsdev@gmail.com>
2025-07-29 23:34:41 +0700

    pati

--- flone/src/wrappers/footer/FooterOne.js
+++ flone/src/wrappers/footer/FooterOne.js
@@ -1,8 +1,6 @@
 import PropTypes from "prop-types";
 import clsx from "clsx";
 import { Link } from "react-router-dom";
-import FooterCopyright from "../../components/footer/FooterCopyright";
-import FooterNewsletter from "../../components/footer/FooterNewsletter";
 
 
 const FooterOne = ({
@@ -16,33 +14,35 @@ const FooterOne = ({
   sideMenu
 }) => {
   return (
-    <footer className={clsx("footer-area", backgroundColorClass, spaceTopClass, spaceBottomClass, extraFooterClass, spaceLeftClass, spaceRightClass )}>
+    
+    <footer className={clsx("footer-area", backgroundColorClass, spaceTopClass, spaceBottomClass, extraFooterClass, spaceLeftClass, spaceRightClass)}>
       <div className={`${containerClass ? containerClass : "container"}`}>
         <div className="row">
-         
-            <div className="footer-widget mb-30 ml-30">
-              <div className="footer-title">
-                <h3>Alamat</h3>
-              </div>
-              <div className="footer-list">
-                <ul>
-                  <li>
-                    <Link to={"https://g.co/kgs/XCQ1KBT"}>PILANGREJO, Desa/Kelurahan Genengduwur, Kec. Gemolong, Kab. Sragen, Provinsi Jawa Tengah, Kode Pos: 57247</Link>
-                  </li>
-                  <li>
-                    <Link to={"https://wa.me/8112657512"}>
+
+          <div className="footer-widget mb-30 ml-30">
+            <div className="footer-title">
+              <h3>Alamat</h3>
+            </div>
+            <div className="footer-list">
+
+              <ul>
+                <li>
+                  <Link to={"https://g.co/kgs/XCQ1KBT"}>PILANGREJO, Desa/Kelurahan Genengduwur, Kec. Gemolong, Kab. Sragen, Provinsi Jawa Tengah, Kode Pos: 57247</Link>
+                </li>
+                <li>
+                  <Link to={"https://wa.me/8112657512"}>
                     No. Telepon : 08112657512
-                    </Link>
-                  </li>
-                  <li>
-                   
-                  </li>
-                </ul>
-              </div>
+                  </Link>
+                </li>
+                <li>
+
+                </li>
+              </ul>
             </div>
           </div>
-        
-          
+        </div>
+
+
       </div>
     </footer>
   );
